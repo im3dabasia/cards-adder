14ff94e1b4e6198426ebc103f12de8c3d55103fc
im3dabasia <eshaandabasiya@gmail.com>
2023-07-16 16:27:52 +0530

    Boilerplate for frontend ready now have to add modals and views

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React, { useEffect } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home"
+function App() {
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
