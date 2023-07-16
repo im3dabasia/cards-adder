1535ac57c8df8eb7973b51d7491c4461beb0e0c1
im3dabasia <eshaandabasiya@gmail.com>
2023-07-16 15:40:47 +0530

    Made the post new user controller and route to add a new user

--- Controllers/User.js
+++ Controllers/User.js
@@ -4,9 +4,38 @@ const UserController = {
 
     getAllUsers: async (req, res) => {
         // console.log("new sending users")
-        const allusers = await User.find({}).populate("position");
 
-        res.json(allusers)
+        try {
+            const allusers = await User.find({}).populate("position");
+            res.json(allusers)
+            
+        } catch (error) {
+            res.status(500).json({ error: 'An error occurred' });
+        }
+    },
+    postNewUser: async (req, res) => {
+        // console.log("making new users")
+        try {
+
+            const newUser = new User({
+                firstName: req.body.username,
+                lastName: req.body.password,
+                email: req.body.email,
+                startDate: req.body.startDate,
+                position: req.body.position
+            });
+            newUser.save()
+
+            res.json({
+                Message: "User Added Successfully",
+                Position: newUser
+            });
+
+        } catch (error) {
+            res.status(500).json({ error: 'An error occurred' });
+        }
+
+
 
     }
 }
--- Routes/User.js
+++ Routes/User.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const UserController = require("../Controllers/User")
 
 router.get('/all',UserController.getAllUsers)
+router.post('/',UserController.postNewUser)
+
 
 module.exports = router;
